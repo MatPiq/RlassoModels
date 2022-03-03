68bbaa6bba0d98173d8c429bfba2d6f036c63cd2
Matias Piqueras <matiaspiqueras.1994@gmail.com>
2022-03-03 15:06:02 +0100

    Initial commit

---
+++ rlassopy/tests/test_common.py
@@ -0,0 +1,11 @@
+import pytest
+from sklearn.utils.estimator_checks import check_estimator
+
+from rlassopy import TemplateClassifier, TemplateEstimator, TemplateTransformer
+
+
+@pytest.mark.parametrize(
+    "estimator", [TemplateEstimator(), TemplateTransformer(), TemplateClassifier()]
+)
+def test_all_estimators(estimator):
+    return check_estimator(estimator)
