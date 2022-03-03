8daae89d0b40319f74e1dacadf9fd687cefd374f
Matias Piqueras <matiaspiqueras.1994@gmail.com>
2022-03-03 19:50:16 +0100

    Update __init__.py

--- rlassopy/__init__.py
+++ rlassopy/__init__.py
@@ -1,8 +1,8 @@
-from ._template import TemplateEstimator
-from ._template import TemplateClassifier
-from ._template import TemplateTransformer
-
 from ._version import __version__
+from .models import Rlasso, SqrtRlasso
 
-__all__ = ['TemplateEstimator', 'TemplateClassifier', 'TemplateTransformer',
-           '__version__']
+__all__ = [
+    "Rlasso",
+    "SqrtRlasso",
+    "__version__",
+]
