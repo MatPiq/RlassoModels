00786d9c30c4e5bb3306d0de60ae5669580dcc81
Matias Piqueras <matiaspiqueras.1994@gmail.com>
2022-03-29 20:02:10 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from glob import glob
 
 import pybind11
 from pybind11.setup_helpers import Pybind11Extension, build_ext
+from setuptools import Extension, setup
 from skbuild import setup
 
-# from setuptools import Extension, setup
 __version__ = "0.0.1"
 
 # linkargs = [
@@ -49,30 +49,30 @@ class get_pybind_include(object):
         return pybind11.get_include(self.user)
 
 
-ext_modules = [
-    Pybind11Extension(
-        "solver_fast",
-        sorted(glob("rlassopy/*.cpp")),
-        include_dirs=[
-            get_pybind_include(),
-            get_pybind_include(user=True),
-            os.environ.get("EIGEN_INCLUDE_DIR", "extern/eigen-3.4.0"),
-        ],
-        define_macros=[("VERSION_INFO", __version__)],
-    ),
-]
-
-# cpp_args = ["-std=c++14"]
-#
 # ext_modules = [
-#     Extension(
+#     Pybind11Extension(
 #         "solver_fast",
 #         sorted(glob("rlassopy/*.cpp")),
-#         include_dirs=[pybind11.get_include(), "extern/eigen-3.4.0"],
-#         language="c++",
-#         extra_compile_args=cpp_args,
+#         include_dirs=[
+#             get_pybind_include(),
+#             get_pybind_include(user=True),
+#             os.environ.get("EIGEN_INCLUDE_DIR", "extern/eigen-3.4.0"),
+#         ],
+#         define_macros=[("VERSION_INFO", __version__)],
 #     ),
 # ]
+
+cpp_args = ["-std=c++14"]
+
+ext_modules = [
+    Extension(
+        "solver_fast",
+        sorted(glob("rlassopy/*.cpp")),
+        include_dirs=[pybind11.get_include(), "extern/eigen-3.4.0"],
+        language="c++",
+        extra_compile_args=cpp_args,
+    ),
+]
 extra_requires = {
     "tests": ["pytest", "pytest-cov"],
     "docs": ["sphinx", "sphinx-gallery", "sphinx_rtd_theme", "numpydoc", "matplotlib"],
