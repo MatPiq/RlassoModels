35937059225a968f1374f5ba0a4d0c7e08c9fa96
Matias Piqueras <matiaspiqueras.1994@gmail.com>
2022-03-29 20:13:35 +0200

    test rtd

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 import os
+import sys
 from glob import glob
+from pathlib import Path
 
 import pybind11
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 from setuptools import Extension, setup
 from skbuild import setup
+
+SETUP_DIRECTORY = Path(__file__).resolve().parent
 
 __version__ = "0.0.1"
 
@@ -34,6 +38,36 @@ __version__ = "0.0.1"
 # ]
 
 
+class get_eigen_include(object):
+    EIGEN3_URL = "https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.zip"
+    EIGEN3_DIRNAME = "eigen-3.4.0"
+
+    def __str__(self) -> str:
+        eigen_include_dir = os.environ.get("EIGEN3_INCLUDE_DIR", None)
+
+        if eigen_include_dir is not None:
+            return eigen_include_dir
+
+        target_dir = SETUP_DIRECTORY / self.EIGEN3_DIRNAME
+        if target_dir.exists():
+            return target_dir.name
+
+        download_target_dir = SETUP_DIRECTORY / "eigen3.zip"
+        import zipfile
+
+        import requests
+
+        response = requests.get(self.EIGEN3_URL, stream=True)
+        with download_target_dir.open("wb") as ofs:
+            for chunk in response.iter_content(chunk_size=1024):
+                ofs.write(chunk)
+
+        with zipfile.ZipFile(download_target_dir) as ifs:
+            ifs.extractall()
+
+        return target_dir.name
+
+
 class get_pybind_include(object):
     """Helper class to determine the pybind11 include path
     The purpose of this class is to postpone importing pybind11
@@ -49,18 +83,19 @@ class get_pybind_include(object):
         return pybind11.get_include(self.user)
 
 
-# ext_modules = [
-#     Pybind11Extension(
-#         "solver_fast",
-#         sorted(glob("rlassopy/*.cpp")),
-#         include_dirs=[
-#             get_pybind_include(),
-#             get_pybind_include(user=True),
-#             os.environ.get("EIGEN_INCLUDE_DIR", "extern/eigen-3.4.0"),
-#         ],
-#         define_macros=[("VERSION_INFO", __version__)],
-#     ),
-# ]
+ext_modules = [
+    Pybind11Extension(
+        "solver_fast",
+        sorted(glob("rlassopy/*.cpp")),
+        include_dirs=[
+            get_pybind_include(),
+            get_pybind_include(user=True),
+            get_eigen_include(),
+            # os.environ.get("EIGEN_INCLUDE_DIR", "extern/eigen-3.4.0"),
+        ],
+        define_macros=[("VERSION_INFO", __version__)],
+    ),
+]
 
 cpp_args = ["-std=c++14"]
 
@@ -92,5 +127,6 @@ setup(
     cmdclass={"build_ext": build_ext},
     install_requires=install_requires,
     extras_require=extra_requires,
+    include_package_data=True,
     zip_safe=False,
 )
