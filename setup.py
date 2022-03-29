8a32a1615c0fc1150400d099017134ef0c0134d9
Matias Piqueras <matiaspiqueras.1994@gmail.com>
2022-03-29 22:55:38 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ ext_modules = [
         include_dirs=[
             get_pybind_include(),
             get_pybind_include(user=True),
-            SETUP_DIRECTORY / "extern/eigen-3.4.0",
+            "extern/eigen-3.4.0",
         ],
         define_macros=[("VERSION_INFO", __version__)],
     ),
