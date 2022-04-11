6137bf36ecb76e600d2466b014f0ba4d065cfe4e
Matias Piqueras <matiaspiqueras.1994@gmail.com>
2022-04-11 14:20:22 +0200

    remove lassoshooting from docs

--- rlassopy/__init__.py
+++ rlassopy/__init__.py
@@ -1,11 +1,8 @@
-from solver_fast import lasso_shooting
-
 from ._version import __version__
 from .estimators import Rlasso, RlassoLogit
 
 __all__ = [
     "Rlasso",
     "RlassoLogit",
-    "lasso_shooting",
     "__version__",
 ]
