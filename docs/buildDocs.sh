19ece081ed20afe0cc2f51faf2645a0f915534f7
Matias Piqueras <matiaspiqueras.1994@gmail.com>
2022-04-24 22:20:06 +0200

    add python doc requirements

--- docs/buildDocs.sh
+++ docs/buildDocs.sh
@@ -18,7 +18,8 @@ set -x
  
 apt-get update
 apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme
- 
+
+pip3 install -r docs/requirements.txt
 #####################
 # DECLARE VARIABLES #
 #####################
