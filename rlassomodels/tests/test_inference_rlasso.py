c98959e9a23028fe8315dd6b350f5ce337e68d17
Matias Piqueras <matiaspiqueras.1994@gmail.com>
2022-04-25 13:30:45 +0200

    updated tests path

---
+++ rlassomodels/tests/test_inference_rlasso.py
@@ -0,0 +1,21 @@
+import numpy as np
+import numpy.linalg as la
+import pandas as pd
+import pytest
+from numpy.testing import assert_allclose, assert_equal
+
+from rlassopy import RlassoIV, RlassoPDS
+
+
+@pytest.fixture
+def ajr_data():
+    return pd.read_stata("https://statalasso.github.io/dta/AJR.dta")
+
+
+def extract_params(model):
+    pds = model.results_["PDS"]
+    chs = model.results_["CHS"]
+    return {
+        "pds_beta": pds.params,
+        "chs_beta": chs.params,
+    }
